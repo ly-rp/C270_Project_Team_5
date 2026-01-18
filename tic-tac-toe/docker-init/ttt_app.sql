0907a569734f92e987a18ed690b4adaef0728aab
ThuNandeAung <24012182@myrp.edu.sg>
2026-01-18 19:11:05 +0800

    Eleanor [7]

--- tic-tac-toe/docker-init/ttt_app.sql
+++ tic-tac-toe/docker-init/ttt_app.sql
@@ -1,4 +1,5 @@
--- Create users table
+USE ttt_app_listweight;
+
 CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) PRIMARY KEY,
     wins INT DEFAULT 0,
@@ -6,9 +7,9 @@ CREATE TABLE IF NOT EXISTS users (
     draws INT DEFAULT 0
 );
 
--- Optional sample data
 INSERT INTO users (username, wins, losses, draws) VALUES
 ('Alice', 0, 0, 0),
 ('Bob', 0, 0, 0);
+
 
 -- 'This is just temporary data, will change everything after the scoring format has been confirmed ^^' -Eleanor
